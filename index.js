f56f0afe42219e924ef97c0f7fce6187d84d07d2
Clara <c_c_456@hotmail.com>
2017-11-09 16:23:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ function changeLeastFavoriteCustomer(){
 
 function attemptTwoFavoriteCustomers() {
 let favoriteCustomer = 'Julie';
-    favoriteCustomer = 'Pavs';
+let  favoriteCustomer = 'Pavs';
 };
